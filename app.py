8c96d9f679ba3d6f64095343e3ec70113aefef75
Dandikarthik <saikarthikdandi4@gmail.com>
2025-05-26 20:37:59 +0530

    fix render

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
 import os
+import psycopg2
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
 CORS(app)
